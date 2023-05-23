f86753b2f04c6d1fb84e2e5586e45262b62a8803
bhavesh9168075778 <134044852+bhavesh9168075778@users.noreply.github.com>
2023-05-23 15:35:08 +0530

    Ems tree added
    first code written in ems tree

--- src/app/emstree/emstree.component.ts
+++ src/app/emstree/emstree.component.ts
@@ -6,7 +6,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emstree.component.scss']
 })
 export class EmstreeComponent implements OnInit {
-
+arr:any = [
+  [
+    {"floor":"Unit 2 Silvasaa","meterName":"Core Lamination area","alert": 3},
+    {"floor":"Unit 2 Silvasaa","meterName":"Bhavesh","alert": 7},
+    {"floor":"1 floor","meterName":"iuhnuv","alert": 2},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 9},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 5}
+  ],
+  [
+    {"floor":"1 floor","meterName":"abcbd","alert": 4},
+    {"floor":"1 floor","meterName":"iuhnuv","alert": 1},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 6},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 8},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 6},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 8},
+    {"floor":"1 floor","meterName":"abcbd","alert": 4},
+    {"floor":"1 floor","meterName":"iuhnuv","alert": 1},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 6},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 8},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 6},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 8}
+  ],
+  [
+    {"floor":"1 floor","meterName":"abcbd","alert": 7},
+    {"floor":"1 floor","meterName":"iuhnuv","alert": 2},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 4},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 3}
+  ],
+  [
+    {"floor":"1 floor","meterName":"abcbd","alert": 9},
+    {"floor":"1 floor","meterName":"iuhnuv","alert": 5},
+    {"floor":"1 floor","meterName":"yuewgw","alert": 8},
+    {"floor":"1 floor","meterName":"diuwifuhf","alert": 1}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 6},
+    {"floor":2,"meterName":"iuhnuv","alert": 9},
+    {"floor":2,"meterName":"yuewgw","alert": 3},
+    {"floor":2,"meterName":"diuwifuhf","alert": 2}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 5},
+    {"floor":2,"meterName":"iuhnuv","alert": 4},
+    {"floor":2,"meterName":"yuewgw","alert": 7},
+    {"floor":2,"meterName":"diuwifuhf","alert": 6}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 1},
+    {"floor":2,"meterName":"iuhnuv","alert": 8},
+    {"floor":2,"meterName":"yuewgw","alert": 2},
+    {"floor":2,"meterName":"diuwifuhf","alert": 5}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 3},
+    {"floor":2,"meterName":"iuhnuv","alert": 7},
+    {"floor":2,"meterName":"yuewgw","alert": 1},
+    {"floor":2,"meterName":"diuwifuhf","alert": 9}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 8},
+    {"floor":2,"meterName":"iuhnuv","alert": 6},
+    {"floor":2,"meterName":"yuewgw","alert": 9},
+    {"floor":2,"meterName":"diuwifuhf","alert": 4}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 2},
+    {"floor":2,"meterName":"iuhnuv","alert": 1},
+    {"floor":2,"meterName":"yuewgw","alert": 5},
+    {"floor":2,"meterName":"diuwifuhf","alert": 8}
+  ],
+  [
+    {"floor":2,"meterName":"abcbd","alert": 7},
+    {"floor":2,"meterName":"iuhnuv","alert": 3},
+    {"floor":2,"meterName":"yuewgw","alert": 4},
+    {"floor":2,"meterName":"diuwifuhf","alert": 6}
+  ]
+]
+;
   constructor() { }
 
   ngOnInit() {
